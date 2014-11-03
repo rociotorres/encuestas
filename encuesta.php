02ae99bbdb3c31d7be84d732bedb0632973e3ff7
rociotorres <rocio.torres@proyeccione.com>
2014-11-03 12:38:06 -0700

    maqueta

---
+++ encuesta.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0 user-scalable=no ">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Concredito Encuesta</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="css/encuesta.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy this line! -->
+    <!--[if lt IE 9]><script src="../../docs-assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body class="backtop">
+    <div class="container">
+        <div class="row text-center">
+          <img src="img/qescucharte.png" class="img-responsive enlinea margen1">
+        </div>
+        <div class="row text-center inter2 margen2">
+          <p class="allerl text2 verde">¿Cómo lo trata <br><span class="text0">la vida?</span></p>
+          
+          
+        </div>
+        <div class="row text-center">
+          <div class="col-xs-2">
+             <img src="img/cara1.png" class="img-responsive enlinea">
+             <p class="gris2 allerb text00 inter3">Nunca se retrasó</p>
+          </div>
+          <div class="col-xs-2">
+            <img src="img/cara2.png" class="img-responsive enlinea">
+             <p class="gris2 allerb text00 inter3">Se retrasó<br>1 vez</p>
+          </div>
+          <div class="col-xs-2">
+            <img src="img/cara3.png" class="img-responsive enlinea">
+             <p class="gris2 allerb text00 inter3">Se retrasó<br>2 veces</p>
+          </div>
+          <div class="col-xs-2">
+            <img src="img/cara4.png" class="img-responsive enlinea">
+             <p class="gris2 allerb text00 inter3">Se retrasó<br>3 veces</p>
+          </div>
+          <div class="col-xs-2">
+            <img src="img/cara5.png" class="img-responsive enlinea">
+             <p class="gris2 allerb text00 inter3">Se retrasó<br>4 veces</p>
+          </div>
+          <div class="col-xs-2">
+            <img src="img/cara6.png" class="img-responsive enlinea">
+             <p class="gris2 allerb text00 inter3">Se retrasó<br>más de 5 veces</p>
+          </div>
+        </div>
+    </div>
+
+    <footer class="row text-center">
+      <img src="img/cclogo.png" class="img-responsive enlinea margen1">
+    </footer>
+
+
+
+  </body>
+  </html>
